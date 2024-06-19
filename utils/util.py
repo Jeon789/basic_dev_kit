d067cdc34f27ac5fc9e5013a4d7435f8bc12aae9
Jae Heyoung Jeon <74503396+Jeon789@users.noreply.github.com>
2024-06-19 16:11:58 +0900

    Update util.py

--- utils/util.py
+++ utils/util.py
@@ -31,6 +31,19 @@ def seed_everything(seed):
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
 
+# lr scheduler for training
+def adjust_learning_rate(optimizer, epoch, args):
+    """Decay the learning rate based on schedule"""
+    lr = args.lr
+    if args.cos:  # cosine lr schedule
+        lr *= 0.5 * (1. + math.cos(math.pi * epoch / args.epochs))
+    else:  # stepwise lr schedule
+        for milestone in args.schedule:
+            lr *= 0.1 if epoch >= milestone else 1.
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
+
+
 class Stdout_Font:
     Reset   ='\033[0m'
     Bold    ='\033[1m'
